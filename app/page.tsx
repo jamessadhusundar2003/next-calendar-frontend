4b99acc7be732d17ecef3e4c386f796f93300519
jamessadhusundar2003 <jamessadhusundar2003+github@gmail.com>
2026-07-15 11:13:22 +0530

    Refactor Home component layout and content
    Removed the default content and added a new layout with FloatingLines and ShinyText components. Updated the structure to include a personal calendar description and a call-to-action button.

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,60 @@
-import Image from "next/image";
+import FloatingLines from "@/components/FloatingLines";
+import ShinyText from "@/components/ShinyText";
+
+import GradientText from "@/components/GradientText"
+
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
+    <div>
+
+      <div style={{
+        width: '100%', height: '100vh', position: 'fixed', top: 0, left: 0, zIndex: 0, background: 'black' }}>
+        <FloatingLines 
+          enabledWaves={["top","middle","bottom"]}
+          // Array - specify line count per wave; Number - same count for all waves
+          lineCount={8}
+          // Array - specify line distance per wave; Number - same distance for all waves
+          lineDistance={8}
+          bendRadius={8}
+          bendStrength={-2}
+          interactive={false}
+          parallax={true}
+          animationSpeed={1}
+          linesGradient={["#A855F7", "#7C3AED", "#6366F1"]}
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+      </div>
+
+      <div style={{ position: 'relative', zIndex: 1, display: 'flex', flexDirection: 'column', justifyContent: 'space-between', alignItems: "center", height: '100vh' }}>
+
+        <div></div>
+
+        <div className="bg-black/50 backdrop-blur-md border border-black/50 rounded-xl w-1/3 p-8">
+          <ShinyText
+            text="NEXT CALENDAR"
+            className="text-4xl font-bold [font-family:Arial] mb-10"
+            speed={3}
+            delay={0}
+            color="#767676"
+            shineColor="#ffffff"
+            spread={120}
+            direction="left"
+            yoyo={false}
+            pauseOnHover={false}
+            disabled={false}
+          />
+
+          <p className="text-gray-400 font-[Arial] [word-spacing:2px] leading-relaxed text-justify text-xl mb-20">A secure <span className="text-gray-300 font-bold">personal calendar</span> designed to help you remember life's most important moments. Save birthdays, anniversaries, memorials, and special occasions, personalize each event, and receive email reminders so every meaningful date is remembered.</p>
+
+          <a href="/signup" target="_blank" className="bg-gradient-to-r from-blue-700 to-purple-600 text-white font-mono px-4 py-2 rounded-lg cursor-pointer inline-block transition-transform duration-300 ease-in-out hover:scale-105">Get started</a>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+
+        <div>
+          <p className="text-gray-400 font-[Arial] mb-2">© {new Date().getFullYear()} James Sadhu Sundar.</p>
         </div>
-      </main>
+        
+      </div>
+
     </div>
   );
 }
