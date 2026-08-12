03746c2ac13ab596d1829b9b7476434d7dca0005
jamessadhusundar2003 <jamessadhusundar2003+github@gmail.com>
2026-08-12 18:51:59 +0530

    Add popover functionality to DateTile component

--- components/calendar/DateTile.tsx
+++ components/calendar/DateTile.tsx
@@ -1,12 +1,54 @@
+import {
+  Popover,
+  PopoverContent,
+  PopoverDescription,
+  PopoverHeader,
+  PopoverTitle,
+  PopoverTrigger,
+} from "@/components/ui/popover"
 
 type DateTileProps = {
     date: (number | null)
+    current_date: boolean
 }
 
 export default function DateTile(props: DateTileProps) {
+    
+    if (props.current_date) {
+        return(
+            <Popover>
+                <PopoverTrigger nativeButton={false} render={
+                    <div className="w-full aspect-square flex items-start justify-start p-3 rounded-xl text-xl font-medium text-white bg-white/10 transition-colors cursor-pointer">
+                        <p>{props.date}</p>
+                    </div>
+                } />
+                <PopoverContent align="start">
+                <PopoverHeader>
+                    <PopoverTitle>Hello</PopoverTitle>
+                    <PopoverDescription>
+                        James Sadhu Sundar - Birthday!
+                    </PopoverDescription>
+                </PopoverHeader>
+                </PopoverContent>
+            </Popover>
+        )
+    }
+
     return(
-        <div className="w-full aspect-square flex items-center justify-center rounded-xl text-sm font-medium text-slate-300 hover:bg-slate-700 hover:text-white transition-colors cursor-pointer">
-            <p>{props.date}</p>
-        </div>
+        <Popover>
+                <PopoverTrigger nativeButton={false} render={
+                    <div className="w-full aspect-square flex items-start justify-start p-3 rounded-xl text-xl font-medium text-neutral-500 hover:bg-white/10 hover:text-white transition-colors cursor-pointer">
+                        <p>{props.date}</p>
+                    </div>
+                } />
+                <PopoverContent align="start">
+                <PopoverHeader>
+                    <PopoverTitle>Hello</PopoverTitle>
+                    <PopoverDescription>
+                        James Sadhu Sundar - Birthday!
+                    </PopoverDescription>
+                </PopoverHeader>
+                </PopoverContent>
+            </Popover>
     )
 }
